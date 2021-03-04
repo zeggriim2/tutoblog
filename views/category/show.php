63c7de488eb83d4b63ded2bcdbd69b171fc6cff0
Zeggriim <51760726+zeggriim2@users.noreply.github.com>
2021-03-04 14:44:53 +0100

    Refactoring de la pagination

--- views/category/show.php
+++ views/category/show.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Connection;
+use App\PaginatedQuery;
 use App\Models\{Category, Post};
 use App\URL;
 
@@ -27,27 +28,19 @@ if ($slug !== $category->getSlug()){
 $title = "Catégorie " . $category->getName();
 
 
-$currentPage = URL::getPositiveInt('page',1);
+$paginatedQuery = new PaginatedQuery(
+        "SELECT p.* 
+                FROM post p
+                JOIN post_category pc ON p.id = pc.post_id
+                WHERE pc.category_id = {$category->getId()}
+        ORDER BY created_at ",
+    "SELECT COUNT(category_id) FROM post_category WHERE category_id = {$category->getId()}"
+);
 
-$count = (int)$pdo
-            ->query("SELECT COUNT(category_id) FROM post_category WHERE category_id = " . $category->getId())
-            ->fetch(PDO::FETCH_NUM)[0];
-$perPage = 12;
-$pages = ceil($count / $perPage);
-if($currentPage > $pages){
-    throw new Exception('Cette page n\'existe pas.');
-}
+/** @var Page $posts */
+$posts = $paginatedQuery->getItems(Post::class);
 
-$offset = $perPage * ($currentPage - 1);
-$query = $pdo->query("
-        SELECT p.* 
-        FROM post p
-        JOIN post_category pc ON p.id = pc.post_id
-        WHERE pc.category_id = {$category->getId()}
-        ORDER BY created_at 
-        DESC LIMIT $perPage OFFSET $offset        
-");
-$posts = $query->fetchAll(PDO::FETCH_CLASS, Post::class);
+
 $link = $router->url('category', ['id' => $category->getId(), 'slug'=> $category->getSlug()]);
 
 ?>
@@ -64,14 +57,6 @@ $link = $router->url('category', ['id' => $category->getId(), 'slug'=> $category
 </div>
 
 <div class="d-flex justify-content-between my-4">
-    <?php if($currentPage > 1): ?>
-        <?php
-        $l = $link;
-        if($currentPage > 2) $l .= '?page=' . ($currentPage - 1);
-        ?>
-        <a href="<?= $l ?>" class="btn btn-primary">&laquo; Page Précédente</a>
-    <?php endif ?>
-    <?php if($currentPage < $pages): ?>
-        <a href="<?= $link ?>?page=<?= $currentPage + 1 ?>" class="btn btn-primary ml-auto">Page Suivante &raquo;</a>
-    <?php endif ?>
+    <?= $paginatedQuery->previousPageLink($link) ?>
+    <?= $paginatedQuery->nextPageLink($link) ?>
 </div>
--- views/post/index.php
+++ views/post/index.php
@@ -3,23 +3,25 @@
 use App\Connection;
 use App\Helpers\Text;
 use App\Models\Post;
+use App\PaginatedQuery;
 use App\URL;
 
 $title = 'blog';
 $pdo = Connection::getPdo();
 
-$currentPage = URL::getPositiveInt('page',1);
+$paginatedQuery = new PaginatedQuery(
+        "SELECT * 
+                FROM post 
+                ORDER BY created_at DESC",
+    "SELECT COUNT(id) 
+                FROM post "
+);
 
-$count = (int)$pdo->query("SELECT COUNT(id) FROM post ")->fetch(PDO::FETCH_NUM)[0];
 
-$perPage = 12;
-$pages = ceil($count / $perPage);
-if($currentPage > $pages){
-    throw new Exception('Cette page n\'existe pas.');
-}
 
-$offset = $perPage * ($currentPage - 1);
-$posts = $pdo->query("SELECT * FROM post ORDER BY created_at DESC LIMIT $perPage OFFSET $offset")->fetchAll(PDO::FETCH_CLASS, Post::class);
+$posts = $paginatedQuery->getItems(Post::class);
+
+$link = $router->url('home');
 ?>
 <h1>Mon blog</h1>
 
@@ -32,14 +34,7 @@ $posts = $pdo->query("SELECT * FROM post ORDER BY created_at DESC LIMIT $perPage
 </div>
 
 <div class="d-flex justify-content-between my-4">
-    <?php if($currentPage > 1): ?>
-        <?php
-        $link = $router->url('home');
-        if($currentPage > 2) $link .= '?page=' . ($currentPage-1);
-        ?>
-        <a href="<?= $link ?>" class="btn btn-primary">&laquo; Page Précédente</a>
-    <?php endif ?>
-    <?php if($currentPage < $pages): ?>
-        <a href="<?= $router->url('home') ?>?page=<?= $currentPage + 1 ?>" class="btn btn-primary ml-auto">Page Suivante &raquo;</a>
-    <?php endif ?>
+    <?= $paginatedQuery->previousPageLink($link)?>
+    <?= $paginatedQuery->nextPageLink($link)?>
+
 </div>
